049bbf340d08e88103deaa0b9eba355c070d0a77
Penterfield <pen_cart@hotmail.co.uk>
2021-04-08 15:53:42 +0100

    changed file

--- build-feedback.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-checkov -d . -o json > results.json
-exitCode=$?
-[[ $exitCode = 0 ]] && state="success" || state="failure"
-echo "##vso[task.setvariable variable=CheckovState;]$state"
-exit $exitCode
